460b6955f21db738242ebe5d416b0cf0ee6f4420
Asmaa Atef <asmaaatefomran@gmail.com>
2026-05-06 23:52:49 +0300

    Better solution for Rotate Box.cpp

--- Day2/Rotate Box.cpp
+++ Day2/Rotate Box.cpp
@@ -49,4 +49,34 @@ public:
          return res;
         
     }
+
+
+//optimal solution using one addition 2d vector only instead of two 
+vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        vector<vector<char>> ans(n, vector<char>(m, '.'));
+
+        for(int i = 0;i<m;i++){
+            int x = n - 1;
+            for(int j = n-1;j>=0;j--){
+                //when ever you fix jem put it at the pointer x
+                if(boxGrid[i][j]=='#'){
+                    ans[x][m-i-1] = '#';
+                    x--;
+                }
+                    //when ever you find wall jump the pointer x to it and put it and start from this point in the next step
+                else if(boxGrid[i][j]=='*'){
+                    ans[j][m-i-1] = '*';
+                    x = j - 1;
+                }
+            }
+        }
+
+        return ans;
+    }
 };
+
+
+
